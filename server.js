f17975bba8cb73c299d2001aa58b88226c3afbd8
Muhammed Furkan AYDIN <enterdarkside@gmail.com>
2017-03-13 08:18:40 +0300

    body parser added

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const exphbs  = require('express-handlebars');
 const routers = require("./router/index");
 const cookieParser = require('cookie-parser');
 const MongoStore = require("connect-mongo")(session);
+const bodyParser = require('body-parser')
 const port = process.env.PORT || 3000;
 
 const app = express();
@@ -17,19 +18,8 @@ app
     store : new MongoStore({ url : "mongodb://127.0.0.1:27017/myblog"}),
     cookie : { maxAge: 180 * 60 * 1000 }
   }))
-  .use((req, res, next)=>{
-    let data="";
-    req.on("data",d=>data+=d.toString());
-    req.on("end",_=>{
-      try{
-          req.body=JSON.parse(data);
-          next();
-      }catch(e){
-          next();
-      }
-    })
-  })
-  .use(express.static("./public")) // for development.
+  .use(bodyParser.json())
+  .use(express.static("./public"))
   .use(cookieParser())
   .engine('.hbs', exphbs({ extname: '.hbs' }))
   .set('view engine', '.hbs')
