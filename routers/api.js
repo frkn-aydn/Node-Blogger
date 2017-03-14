847a591343ca450a80912f250f350cf28d705115
Muhammed Furkan AYDIN <enterdarkside@gmail.com>
2017-03-14 06:01:01 +0300

    login and register added

--- routers/api.js
+++ routers/api.js
@@ -1,8 +1,45 @@
 const express = require("express");
-
+const database = require("./../database/index");
 
 exports.api = express.Router();
 
 exports.api
     .post('/login', (req, res) => {
+        if(!req.body) return res.json({
+            error : "Missing fields !",
+            code : 0
+        })
+        if(!req.body.email || !req.body.password) return res.json({
+            error : "Missing fields !",
+            code: 0
+        })
+        const users = new database.users(req.body.email, req.body.password);
+        if(users.error) return res.json(users)
+        users.login().then(user=>{
+            if(user.error) return res.json(user);
+            req.session.user = user;
+            res.json({
+                result : "ok",
+                code : 1
+            })
+        })
+    })
+    .post("/register", (req, res)=>{
+        if(!req.body) return res.json({
+            error : "Missing fields !",
+            code : 0
+        })
+        if(!req.body.email || !req.body.password) return res.json({
+            error : "Missing fields !",
+            code: 0
+        })
+        const users = new database.users(req.body.email, req.body.password);
+        if(users.error) return res.json(users)
+        users.register(false).then(result=>{
+            if(result.error) return res.json(result);
+            res.json({
+                result : "ok", 
+                code : 1
+            })
+        })
     })
